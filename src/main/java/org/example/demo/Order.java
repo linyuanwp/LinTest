b047a52b4e5912dd421a6e65457a89fa5cc01965
linyuan <linyuanwp@gmail.com>
2024-01-17 10:39:19 -0500

    Add order demo No3

--- src/main/java/org/example/demo/Order.java
+++ src/main/java/org/example/demo/Order.java
@@ -1,14 +1,18 @@
 package org.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Order {
     private final int id;
     private final String description;
+    private final List<LineItem> itemList;
 
     public Order(int id, String description) {
         this.id = id;
         this.description = description;
+        itemList = new ArrayList<>();
     }
 
     @Override
@@ -33,6 +37,13 @@ public class Order {
 
     public String getDescription() {
         return description;
+    }
+
+    public double getPrice() {
+//        List<LineItem> itemList = new ArrayList<>();
+        return itemList.stream()
+                .mapToDouble(LineItem::getPrice)
+                .sum();
     }
 
 }
